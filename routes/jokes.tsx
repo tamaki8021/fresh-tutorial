b308d0f4c5b0b66bee3d3aed78decbfe8e67da16
tamaki8021 <hbk2002320@gmail.com>
2022-07-24 02:43:16 +0900

    :sparkles: envelomentの設定

--- routes/jokes.tsx
+++ routes/jokes.tsx
@@ -1,15 +1,21 @@
 /** @jsx h */
 import { h } from "preact";
 import { Handlers, PageProps } from "$fresh/server.ts";
+import "dotenv";
 
 export const handler: Handlers<string> = {
   async GET(_, ctx) {
-    const resp = await fetch("http://localhost:8000/api/joke");
+    const host = Deno.env.get("HOST");
+    const resp = await fetch(`${host}/api/joke`);
     const joke = await resp.text();
     return ctx.render(joke);
   },
 };
 
 export default function jokes({ data }: PageProps<string>) {
-  return <div>{data}</div>;
+  return (
+    <div>
+      {data}
+    </div>
+  );
 }
