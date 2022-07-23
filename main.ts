4d6acdd911194ff41b77596e516a590adaecae22
tamaki8021 <hbk2002320@gmail.com>
2022-07-23 21:52:06 +0900

    feat: new fresh

--- main.ts
+++ main.ts
@@ -1,5 +1,26 @@
-import { serve } from "./deps.ts";
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
 
-serve((req) => new Response("Hello world from Deno!"), { addr: ":8000" });
+import { InnerRenderFunction, RenderContext, start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
 
-console.log(`HTTP server is running at: http://localhost:8080/`);
+import { config, setup } from "@twind";
+import { virtualSheet } from "twind/sheets";
+
+const sheet = virtualSheet();
+sheet.reset();
+setup({ ...config, sheet });
+
+function render(ctx: RenderContext, render: InnerRenderFunction) {
+  const snapshot = ctx.state.get("twind") as unknown[] | null;
+  sheet.reset(snapshot || undefined);
+  render();
+  ctx.styles.splice(0, ctx.styles.length, ...(sheet).target);
+  const newSnapshot = sheet.reset();
+  ctx.state.set("twind", newSnapshot);
+}
+
+await start(manifest, { render });
