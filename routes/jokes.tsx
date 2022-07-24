76482895b10a37d513ecc93dcceb8f5a2c89ab7d
tamaki8021 <hbk2002320@gmail.com>
2022-07-24 23:20:30 +0900

    modify: error処理を追加

--- routes/jokes.tsx
+++ routes/jokes.tsx
@@ -5,15 +5,22 @@ import "dotenv";
 
 const host = Deno.env.get("HOST");
 
-export const handler: Handlers<string> = {
+export const handler: Handlers<string | null> = {
   async GET(_, ctx) {
     const resp = await fetch(`${host}/api/joke`);
     const joke = await resp.text();
+    if (resp.status === 404) {
+      return ctx.render(null);
+    }
     return ctx.render(joke);
   },
 };
 
-export default function jokes({ data }: PageProps<string>) {
+export default function jokes({ data }: PageProps<string | null>) {
+  if (!data) {
+    return <h1>not data</h1>;
+  }
+
   return (
     <div>
       {data}
